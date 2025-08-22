7cb4b90a7e44dd5ab0a35290343862ef4aaaf446
JesusLangarica <caaalmvf@gmail.com>
2025-08-22 15:57:12 -0600

    zustand1

--- ssme-frontend/src/stores/useClinicalRecordFormStore.ts
+++ ssme-frontend/src/stores/useClinicalRecordFormStore.ts
@@ -4,18 +4,25 @@ interface FormState {
   patientName: string;
   symptoms: string;
   aiSummary: string;
-  isSummarizing: boolean; // Nuevo estado para la UI de inferencia
-  setField: (field: keyof Omit<FormState, 'setField' | 'setIsSummarizing' | 'resetForm'>, value: string) => void;
+  isSummarizing: boolean;
+}
+
+interface FormActions {
+  setField: (field: keyof Omit<FormState, 'isSummarizing'>, value: string) => void;
   setIsSummarizing: (status: boolean) => void;
   resetForm: () => void;
 }
 
-export const useClinicalRecordFormStore = create<FormState>((set) => ({
+const initialState: FormState = {
   patientName: '',
   symptoms: '',
   aiSummary: '',
   isSummarizing: false,
+};
+
+export const useClinicalRecordFormStore = create<FormState & FormActions>((set) => ({
+  ...initialState,
   setField: (field, value) => set({ [field]: value }),
   setIsSummarizing: (status) => set({ isSummarizing: status }),
-  resetForm: () => set({ patientName: '', symptoms: '', aiSummary: '' }),
+  resetForm: () => set(initialState),
 }));
